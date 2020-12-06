f5683eef7c8a48b006155dc896058f313720db2f
gzstyp <444141300@qq.com>
2020-12-06 13:18:00 +0800

    vertx的框架代码

--- src/main/java/dwz/cloud/Launcher.java
+++ src/main/java/dwz/cloud/Launcher.java
@@ -26,7 +26,16 @@ public class Launcher extends AbstractVerticle {
     methods.add(HttpMethod.POST);
     router.route().blockingHandler(CorsHandler.create("http://192.168.3.108").allowCredentials(true).allowedHeader("content-type").maxAgeSeconds(86400).allowedMethods(methods));
 
-    //第三步,将router和 HttpServer 绑定[若是使用配置文件则这样实例化,如果不配置文件则把它挪动到lambda外边即可]
+    //第三步,配置Router解析url
+    router.route("/").blockingHandler(context -> {
+      ToolClient.responseJson(context,ToolClient.createJson(200,"操作成功"));
+    });
+
+    router.route("/login").blockingHandler(context -> {
+      ToolClient.responseJson(context,ToolClient.createJson(200,"登录成功","vert.x提供服务"));
+    });
+
+    //第四步,将router和 HttpServer 绑定[若是使用配置文件则这样实例化,如果不配置文件则把它挪动到lambda外边即可]
     server.requestHandler(router).listen(89,http -> {
       if (http.succeeded()){
         startPromise.complete();
@@ -35,14 +44,6 @@ public class Launcher extends AbstractVerticle {
         //startPromise.fail(http.cause());
         System.out.println("---Launcher应用启动失败---"+http.cause());
       }
-    });
-    //第四步,配置Router解析url
-    router.route("/").blockingHandler(context -> {
-      ToolClient.responseJson(context,ToolClient.createJson(200,"操作成功"));
-    });
-
-    router.route("/login").blockingHandler(context -> {
-      ToolClient.responseJson(context,ToolClient.createJson(200,"登录成功","vert.x提供服务"));
     });
   }
 }
